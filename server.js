c47d3a51b3bd1b66f88fe27fbffa47c90f228f27
A Bevers <alisabevers@gmail.com>
2023-05-15 17:38:54 -0400

    connected models folder to server. added 2 pieces of data in muscleGroupData seeds file to ensure it seeds the db

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const sequelize = require('./config/connection');
 const path = require("path");
 const exphbs = require('express-handlebars');
-
+const models = require('./models');
 
 const PORT = process.env.PORT || 3001;
 const app = express();
