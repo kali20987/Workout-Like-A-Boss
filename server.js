60ac19ff38291387949fd70f4b02725f602c3902
A Bevers <alisabevers@gmail.com>
2023-05-16 14:28:25 -0400

    revised server.js

--- server.js
+++ server.js
@@ -18,8 +18,10 @@ app.set("view engine", "handlebars");
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(routes);
+
 sequelize.sync({force: false}).then(() => {
     app.listen(PORT, () => {
         console.log('Server is opened');
     });
 });
+
