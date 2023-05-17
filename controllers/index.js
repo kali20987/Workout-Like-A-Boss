018a402c7e68e3cfdef73dcfe1ae68633b29c0bb
A Bevers <alisabevers@gmail.com>
2023-05-17 09:50:48 -0400

    got the review handlebar page to render in browser

--- controllers/index.js
+++ controllers/index.js
@@ -1,6 +1,11 @@
 const router = require('express').Router();
+const review = require('./api/reviewRoutes')
 const pages = require('./pages');
 
+// localhost:3001/
 router.use('/', pages);
+
+// localhost:3001/api
+router.use('/api', review);
 
 module.exports = router;
